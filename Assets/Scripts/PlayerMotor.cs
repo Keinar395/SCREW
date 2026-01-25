4b3f3ce1e53741e2267750cc64b44a46f8271665
Keinar395 <162704682+Keinar395@users.noreply.github.com>
2026-01-25 23:09:19 +0300

    Jump eklendi

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -4,7 +4,10 @@ public class PlayerMotor : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
+    private bool isGrounded;
     public float speed = 5f;
+    public float gravity = -3f;
+    public float jumpHeight = 3000f;
 
     void Start()
     {
@@ -14,7 +17,7 @@ public class PlayerMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = controller.isGrounded;
     }
 
     public void ProcessMove(Vector2 input)
@@ -23,5 +26,20 @@ public class PlayerMotor : MonoBehaviour
         moveDirection.x = input.x;
         moveDirection.z = input.y;
         controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime); 
+        playerVelocity.y += gravity * Time.deltaTime;
+
+        if(isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    public void Jump()
+    {
+        if(isGrounded)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight  * Time.deltaTime);
+        }
     }
 }
